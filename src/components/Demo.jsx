6ea80efd29406584cdc131caa3d30d580bdd8ad1
DejanSheki <dejan.sheki.lukic@gmail.com>
2024-02-20 17:34:49 +0100

    update

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,23 +1,25 @@
 import { useState, useEffect } from "react";
 
 import { copy, linkIcon, loader, tick } from "../assets";
-import { useLazyGetSummaryQuery } from '../services/article';
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
   const [article, setArticle] = useState({
-    url: '',
-    summary: '',
+    url: "",
+    summary: "",
   });
   const [allArticles, setAllArticles] = useState([]);
-  const [copied, setCopied] = useState('');
+  const [copied, setCopied] = useState("");
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
   useEffect(() => {
-    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'));
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles"),
+    );
 
     if (articlesFromLocalStorage) {
-      setAllArticles(articlesFromLocalStorage)
+      setAllArticles(articlesFromLocalStorage);
     }
   }, []);
 
@@ -32,31 +34,30 @@ const Demo = () => {
       setArticle(newArticle);
       setAllArticles(updatedAllArticles);
 
-      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
-
-      setArticle({
-        url: '',
-        summary: '',
-      });
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
-  }
+  };
 
   const handleCopy = (copyUrl) => {
     setCopied(copyUrl);
     navigator.clipboard.writeText(copyUrl);
     setTimeout(() => setCopied(false), 3000);
-  }
+  };
 
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
-        <form className="relative flex justify-center items-center"
+        <form
+          className="relative flex justify-center items-center"
           onSubmit={handleSubmit}
         >
-          <img src={linkIcon} alt="link_icon"
+          <img
+            src={linkIcon}
+            alt="link_icon"
             className="absolute left-0 my-2 ml-3 w-5"
           />
-          <input type="url"
+          <input
+            type="url"
             placeholder="Enter a URL"
             value={article.url}
             onChange={(e) => setArticle({ ...article, url: e.target.value })}
@@ -82,11 +83,13 @@ const Demo = () => {
               <div className="copy_btn" onClick={() => handleCopy(item.url)}>
                 <img
                   src={copied === item.url ? tick : copy}
-                  alt='copy_icon'
+                  alt="copy_icon"
                   className="w-[40%] h-[40%] object-contain"
                 />
               </div>
-              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
             </div>
           ))}
         </div>
@@ -96,23 +99,30 @@ const Demo = () => {
         {isFetching ? (
           <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
         ) : error ? (
-          <p className="font-bold font-inter text-black text-center">Well, that wasn't supposed to happen...
+          <p className="font-bold font-inter text-black text-center">
+            Well, that wasn't supposed to happen...
             <br />
-            <span className="font-satoshi font-normal text-gray-700">{error?.data?.error}</span>
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
           </p>
         ) : (
           article.summary && (
             <div className="flex flex-col ap-3">
-              <h2 className="font-satoshi font-bold text-gray-600 text-xl">Article <span className="blue_gradient">Summary</span></h2>
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
               <div className="summary_box">
-                <p className="font-inter font-medium text-sm text-gray-700">{article.summary}</p>
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
               </div>
             </div>
           )
         )}
       </div>
     </section>
-  )
+  );
 };
 
 export default Demo;
